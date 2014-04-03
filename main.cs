e11a178ab52190dc27248188ec090e974b01a495
larsbrubaker <larsbrubaker@gmail.com>
2014-04-03 16:04:32 -0700

    Including the correct lgpl text in headers.

--- main.cs
+++ main.cs
@@ -5,7 +5,7 @@ Copyright (c) 2014, Lars Brubaker
 This file is part of MatterSlice.
 
 MatterSlice is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
