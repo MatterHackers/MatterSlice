bb6bf79886d07e2ddbeeced129eac727410587ae
Lars Brubaker <larsbrubaker@gmail.com>
2014-12-11 15:08:15 -0800

    Updated the license to agpl.

--- bridge.cs
+++ bridge.cs
@@ -1,21 +1,22 @@
 /*
+This file is part of MatterSlice. A commandline utility for 
+generating 3D printing GCode.
+
 Copyright (C) 2013 David Braam
 Copyright (c) 2014, Lars Brubaker
 
-This file is part of MatterSlice.
-
 MatterSlice is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-MatterSlice is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with MatterSlice.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 //#define OUTPUT_DEBUG_DATA
 
