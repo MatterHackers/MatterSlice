4443f9ccc9ed30538da19e35c00d91a8caea7d2b
larsbrubaker <larsbrubaker@gmail.com>
2015-11-19 07:45:19 -0800

    More thinking about new supports.

--- NewSupport.cs
+++ NewSupport.cs
@@ -30,17 +30,21 @@ namespace MatterHackers.MatterSlice
 	public class NewSupport
 	{
         readonly static double cleanDistance_um = 10;
-        public List<Polygons> supportPolygons = new List<Polygons>();
+        List<Polygons> allPartOutlines = new List<Polygons>();
+        List<Polygons> allPossibleSupportOutlines = new List<Polygons>();
+        List<Polygons> minimumSupportOutlines = new List<Polygons>();
 
         public NewSupport(int numLayers, ConfigSettings config, PartLayers storage)
         {
             for (int i = 0; i < numLayers; i++)
             {
-                supportPolygons.Add(null);
+                allPossibleSupportOutlines.Add(null);
+                allPartOutlines.Add(null);
             }
 
-			// create starting support outlines
-			FindAllPossibleSupportOutlines(numLayers, config, storage);
+            // create starting support outlines
+            CalculateAllPartOutlines(numLayers, config, storage);
+            FindAllPossibleSupportOutlines(numLayers, config, storage);
 			RemoveSelfSupportedSections(numLayers, config, storage);
 			// clip to xy distance from all parts
 			// change top layers into interface layers
@@ -55,124 +59,57 @@ namespace MatterHackers.MatterSlice
 		}
 
 
-		private void FindAllPossibleSupportOutlines(int numLayers, ConfigSettings config, PartLayers storage)
+        private void CalculateAllPartOutlines(int numLayers, ConfigSettings config, PartLayers storage)
+        {
+            // calculate the combind outlines for everything
+            for (int layerIndex = numLayers - 1; layerIndex >= 0; layerIndex--)
+            {
+                Polygons allOutlines = new Polygons();
+
+                SliceLayerParts curLayer = storage.Layers[layerIndex];
+                for (int curLayerPartIndex = 0; curLayerPartIndex < curLayer.parts.Count; curLayerPartIndex++)
+                {
+                    SliceLayerPart curPart = curLayer.parts[curLayerPartIndex];
+                    Polygons curLayerPolys = curPart.TotalOutline;
+                    allOutlines = allOutlines.CreateUnion(curLayerPolys);
+                    allOutlines = Clipper.CleanPolygons(allOutlines, cleanDistance_um);
+                }
+
+                allPartOutlines[layerIndex] = allOutlines.DeepCopy();
+            }
+        }
+
+        private void FindAllPossibleSupportOutlines(int numLayers, ConfigSettings config, PartLayers storage)
         {
             // calculate all the non-supported areas
             for (int layerIndex = numLayers - 2; layerIndex >= 0; layerIndex--)
             {
-				Polygons allSupportAreas = new Polygons();
-
-				SliceLayerParts aboveLayer = storage.Layers[layerIndex + 1];
-                SliceLayerParts curLayer = storage.Layers[layerIndex];
-				for (int aboveLayerPartIndex = 0; aboveLayerPartIndex < aboveLayer.parts.Count; aboveLayerPartIndex++)
-				{
-					SliceLayerPart abovePart = aboveLayer.parts[aboveLayerPartIndex];
-					Polygons aboveLayerPolys = abovePart.TotalOutline;
-					if (curLayer.parts.Count > 0)
-					{
-						for (int curLayerPartIndex = 0; curLayerPartIndex < curLayer.parts.Count; curLayerPartIndex++)
-						{
-							SliceLayerPart curPart = curLayer.parts[curLayerPartIndex];
-							Polygons curLayerPolys = curPart.TotalOutline;
-							aboveLayerPolys = aboveLayerPolys.CreateDifference(curLayerPolys.Offset(10));
-						}
-					}
-
-					allSupportAreas = allSupportAreas.CreateUnion(aboveLayerPolys);
-					allSupportAreas = Clipper.CleanPolygons(allSupportAreas, cleanDistance_um);
-				}
-
-				supportPolygons[layerIndex] = allSupportAreas.DeepCopy();
+                Polygons aboveLayerPolys = allPartOutlines[layerIndex+1];
+                Polygons curLayerPolys = allPartOutlines[layerIndex];
+                Polygons supportedAreas = aboveLayerPolys.CreateDifference(curLayerPolys.Offset(10));
+                allPossibleSupportOutlines[layerIndex] = Clipper.CleanPolygons(supportedAreas, cleanDistance_um);
             }
         }
 
-		private void RemoveSelfSupportedSections(int numLayers, ConfigSettings config, PartLayers storage)
+        private void RemoveSelfSupportedSections(int numLayers, ConfigSettings config, PartLayers storage)
 		{
 			// calculate all the non-supported areas
-			for(int i=0; i<supportPolygons.Count; i++)
+			for(int i=0; i<allPossibleSupportOutlines.Count; i++)
 			{
-				if (supportPolygons[i] != null)
+				if (allPossibleSupportOutlines[i] != null)
 				{
-					supportPolygons[i] = supportPolygons[i].Offset(-config.extrusionWidth_um / 2);
-					supportPolygons[i] = supportPolygons[i].Offset(config.extrusionWidth_um / 2);
+					allPossibleSupportOutlines[i] = allPossibleSupportOutlines[i].Offset(-config.extrusionWidth_um / 2);
+					allPossibleSupportOutlines[i] = allPossibleSupportOutlines[i].Offset(config.extrusionWidth_um / 2);
 				}
 			}
 		}
-
-		private void WorkingTotal(int numLayers, ConfigSettings config, PartLayers storage)
-		{
-			int selfSupportDistance = config.outsideExtrusionWidth_um / 2;
-
-			Polygons accumulatedBottoms = new Polygons();
-
-			// calculate all the non-supported areas
-			for (int layerIndex = numLayers - 1; layerIndex > 0; layerIndex--)
-			{
-				SliceLayerParts belowLayer = storage.Layers[layerIndex - 1];
-				SliceLayerParts curLayer = storage.Layers[layerIndex];
-				for (int partIndex = 0; partIndex < curLayer.parts.Count; partIndex++)
-				{
-					SliceLayerPart curPart = curLayer.parts[partIndex];
-					Polygons curLayerPolys = curPart.TotalOutline.Offset(-config.outsideExtrusionWidth_um / 2);
-					if (belowLayer.parts.Count > 0)
-					{
-						SliceLayerPart belowPart = belowLayer.parts[0];
-						Polygons belowLayerPolys = belowPart.TotalOutline;//.Offset(-config.outsideExtrusionWidth_um / 2);
-						Polygons activeLayerPolys = curLayerPolys.CreateDifference(belowLayerPolys);
-
-						// remove anything that is less than the selfSupportDistance
-						{
-							//Polygons noSelfSupportPolys = activeLayerPolys.Offset(-selfSupportDistance);
-							// expand it back out
-							//activeLayerPolys = noSelfSupportPolys.Offset(selfSupportDistance);
-						}
-
-						// remove any portion that will be sitting on existing parts
-						{
-							accumulatedBottoms = accumulatedBottoms.CreateDifference(curLayerPolys);
-							accumulatedBottoms = Clipper.CleanPolygons(accumulatedBottoms, cleanDistance_um);
-						}
-
-						accumulatedBottoms = accumulatedBottoms.CreateUnion(activeLayerPolys);
-						accumulatedBottoms = Clipper.CleanPolygons(accumulatedBottoms, cleanDistance_um);
-					}
-					else // have to add everything from this layer
-					{
-						accumulatedBottoms = accumulatedBottoms.CreateUnion(curLayerPolys);
-						accumulatedBottoms = Clipper.CleanPolygons(accumulatedBottoms, cleanDistance_um);
-					}
-				}
-
-				supportPolygons[layerIndex] = new Polygons();
-				foreach (Polygon poly in accumulatedBottoms)
-				{
-					supportPolygons[layerIndex].Add(new Polygon(poly));
-				}
-			}
-		}
-
-		private void RemoveExistingIntersection(int numLayers, PartLayers storage)
-        {
-            for (int layerIndex = numLayers - 1; layerIndex > 0; layerIndex--)
-            {
-                SliceLayerParts curLayer = storage.Layers[layerIndex];
-                for (int partIndex = 0; partIndex < curLayer.parts.Count; partIndex++)
-                {
-                    SliceLayerPart curPart = curLayer.parts[partIndex];
-                    Polygons curLayerPolys = curPart.TotalOutline;
-
-                    supportPolygons[layerIndex] = supportPolygons[layerIndex].CreateDifference(curLayerPolys);
-                    supportPolygons[layerIndex] = Clipper.CleanPolygons(supportPolygons[layerIndex], cleanDistance_um);
-                }
-            }
-        }
 
         internal void AddSupportToGCode(GCodePlanner gcodeLayer, int layerIndex, GCodePathConfig supportNormalConfig)
         {
-            if (supportPolygons[layerIndex] != null)
+            if (allPossibleSupportOutlines[layerIndex] != null)
             {
                 // make sure last top layer is correct height to leave air gap
-                gcodeLayer.WritePolygonsByOptimizer(supportPolygons[layerIndex], supportNormalConfig);
+                gcodeLayer.WritePolygonsByOptimizer(allPossibleSupportOutlines[layerIndex], supportNormalConfig);
             }
         }
     }
