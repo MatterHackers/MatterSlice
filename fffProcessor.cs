834e3f133386c0ed1d909edab8de5a75eb0635e1
Lars Brubaker <larsbrubaker@gmail.com>
2016-01-20 13:59:40 -0800

    Check the layer count.

--- fffProcessor.cs
+++ fffProcessor.cs
@@ -571,12 +571,15 @@ namespace MatterHackers.MatterSlice
 					// Figure out where the seam hiding start point is for inset 0 and move to that spot so
 					// we have the minimum travel while starting inset 0 after printing the rest of the insets
 					SliceLayer layer = slicingData?.Extruders?[0]?.Layers?[layerIndex + 1];
-					LayerIsland island = layer?.Islands?[0];
-					if (island?.InsetToolPaths?[0]?[0]?.Count > 0)
+					if (layer.Islands.Count > 0)
 					{
-						int bestPoint = PathOrderOptimizer.GetBestEdgeIndex(island.InsetToolPaths[0][0]);
-						gcodeLayer.SetOuterPerimetersToAvoidCrossing(island.AvoidCrossingBoundery);
-						gcodeLayer.QueueTravel(island.InsetToolPaths[0][0][bestPoint]);
+						LayerIsland island = layer?.Islands?[0];
+						if (island?.InsetToolPaths?[0]?[0]?.Count > 0)
+						{
+							int bestPoint = PathOrderOptimizer.GetBestEdgeIndex(island.InsetToolPaths[0][0]);
+							gcodeLayer.SetOuterPerimetersToAvoidCrossing(island.AvoidCrossingBoundery);
+							gcodeLayer.QueueTravel(island.InsetToolPaths[0][0][bestPoint]);
+						}
 					}
 				}
 
