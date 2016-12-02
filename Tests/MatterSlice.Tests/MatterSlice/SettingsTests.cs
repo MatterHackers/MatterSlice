e14e7250c2656e8f74faabf316458db730d71726
Lars Brubaker <larsbrubaker@gmail.com>
2016-12-01 16:32:09 -0800

    updating tests

--- Tests/MatterSlice.Tests/MatterSlice/SettingsTests.cs
+++ Tests/MatterSlice.Tests/MatterSlice/SettingsTests.cs
@@ -130,7 +130,7 @@ namespace MatterHackers.MatterSlice.Tests
 		}
 		#endregion
 
-		[Test, Category("FixNeeded")]
+		[Test, Category("WorkInProgress")]
 		public void AllInsidesBeforeAnyOutsides()
 		{
 			string thinAttachStlFile = TestUtlities.GetStlPath("Thin Attach");
