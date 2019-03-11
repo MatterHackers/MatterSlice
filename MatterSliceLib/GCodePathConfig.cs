09f1c374cf4293da7fb1d1d7bedcd573cac773d5
John Lewin <lewin76@gmail.com>
2019-03-10 20:50:05 -0700

    Require gcodeComment in constructor

--- MatterSliceLib/GCodePathConfig.cs
+++ MatterSliceLib/GCodePathConfig.cs
@@ -33,7 +33,7 @@ namespace MatterHackers.MatterSlice
 		public double Speed { get; private set; }
 		public bool spiralize;
 
-		public GCodePathConfig(string configName, string gcodeComment = "", bool closedLoop = true)
+		public GCodePathConfig(string configName, string gcodeComment, bool closedLoop = true)
 		{
 			this.Name = configName;
 			this.gcodeComment = gcodeComment;
