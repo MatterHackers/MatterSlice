b35dfe02a31e3d3c328ab68ebd4c99f50d7759c7
Lars Brubaker <larsbrubaker@gmail.com>
2021-01-25 13:14:54 -0800

    update version

--- MatterSliceLib/ConfigSettings.cs
+++ MatterSliceLib/ConfigSettings.cs
@@ -32,7 +32,7 @@ namespace MatterHackers.MatterSlice
 	public class ConfigConstants
 	{
 		public const int MAX_EXTRUDERS = 4;
-		public const string VERSION = "2.21.1";
+		public const string VERSION = "2.21.2";
 
 		public enum INFILL_TYPE
 		{
