abac86cdeb4b42cf2c0abf7ae38eb5385fc5f448
Lars Brubaker <larsbrubaker@gmail.com>
2021-09-22 17:52:44 -0700

    refactoring

---
+++ MatterSliceLib/InsetOrderCalculator.cs
@@ -0,0 +1,27 @@
+﻿/*
+This file is part of MatterSlice. A command line utility for
+generating 3D printing GCode.
+
+Copyright (C) 2013 David Braam
+Copyright (c) 2021, Lars Brubaker
+
+MatterSlice is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace MatterHackers.MatterSlice
+{
+	public class InsetOrderCalculator
+	{
+	}
+}
