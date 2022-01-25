f406b043f95c2f654f990e557abb5618c2233d3a
Lars Brubaker <larsbrubaker@gmail.com>
2022-01-24 16:14:32 -0800

    Update version number

--- MatterSliceLib/ConfigSettings.cs
+++ MatterSliceLib/ConfigSettings.cs
@@ -51,7 +51,7 @@ namespace MatterHackers.MatterSlice
 	public class ConfigConstants
 	{
 		public const int MAX_EXTRUDERS = 4;
-		public const string VERSION = "2.21.12";
+		public const string VERSION = "2.22.02";
 	}
 
 	// all the variables in this class will be saved and loaded from settings files
